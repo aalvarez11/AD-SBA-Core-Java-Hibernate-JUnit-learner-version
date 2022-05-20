ae70787df438595a10b8c4045581a4d518dc7bdc
aalvarez11 <aalvarez1@email.arizona.edu>
2022-05-20 15:20:15 -0700

    add tests to StudentServiceTest

--- src/test/java/sba/sms/services/StudentServiceTest.java
+++ src/test/java/sba/sms/services/StudentServiceTest.java
@@ -2,8 +2,8 @@ package sba.sms.services;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import sba.sms.models.Course;
 import sba.sms.models.Student;
 import sba.sms.utils.CommandLine;
 
@@ -14,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudentServiceTest {
 
     static StudentService studentService;
@@ -25,8 +26,8 @@ class StudentServiceTest {
     }
 
     @Test
+    @Order(1)
     void getAllStudents() {
-
         List<Student> expected = new ArrayList<>(Arrays.asList(
                 new Student("reema@gmail.com", "reema brown", "password"),
                 new Student("annette@gmail.com", "annette allen", "password"),
@@ -36,6 +37,28 @@ class StudentServiceTest {
         ));
 
         assertThat(studentService.getAllStudents()).hasSameElementsAs(expected);
+    }
 
+    @Test
+    @Order(2)
+    void createStudent() {
+        studentService.createStudent(new Student("aalvarez@gmail.com", "alejandro alvarez", "password"));
+
+        assertThat(studentService.getAllStudents()).isNotEmpty().hasSize(6);
+    }
+
+    @Test
+    @Order(3)
+    void getStudentByEmail() {
+        assertThat(studentService.getStudentByEmail("annette@gmail.com")).extracting(Student::getName).isEqualTo("annette allen");
+        assertThat(studentService.getStudentByEmail("reema@gmail.com")).extracting(Student::getName).isEqualTo("reema brown");
+        assertThat(studentService.getStudentByEmail("bolaji@gmail.com")).extracting(Student::getPassword).isEqualTo("password");
+    }
+
+    @Test
+    @Order(4)
+    void validateStudent() {
+        assertThat(studentService.validateStudent("anthony@gmail.com", "password")).isTrue();
+        assertThat(studentService.validateStudent("ariadna@gmail.com", "PASSw0rd")).isFalse();
     }
 }
